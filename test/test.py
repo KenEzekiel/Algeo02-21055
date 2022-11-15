0eb607291c7d334d0d01a00d4c39434d1d8bda4f
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2022-11-15 12:02:24 +0700

    fix bugs, add new feature (still not working)

--- test/test.py
+++ test/test.py
@@ -23,25 +23,66 @@ mean = numpy.divide(sum, count)
 for d in data:
     d = numpy.subtract(d, mean)
 
-# data N^2 x M
+# ini kenapa jadi M x N^2? pas di add, di add ke row ya?
+# data M x N^2
 # A = [a1, a2, .., am], A = data
 npdata = numpy.matrix(data)
-CAksen = numpy.matmul(npdata.transpos, npdata.transpose())
-w, v = numpy.linalg.eig(CAksen)
+print(npdata.shape)
+# A : N^2 x M
+npdata = npdata.transpose()
+print('1')
+# A : N^2 x M, At : M x N^2
+# M x M : At x A
+Cov = numpy.matmul(npdata.transpose(), npdata)
+print('2')
+
+w, v = numpy.linalg.eig(Cov)
+print("w")
+print(w)
+print("v")
+print(v.shape)
+print(v)
+print('3')
 
 # Now we select k = 5 of w ( max eigenvalues )
-eigvals = numpy.array([])
-eigvecs = numpy.array([])
 k = 5
-for i in range(k):
-    a = w.max()
-    numpy.append(eigvals, a)
-    index = w.argmax()
-    w = numpy.delete(w, index)
-    vec = v[index]
-    numpy.append(eigvecs, [vec])
+eigvals = numpy.array([])
+eigvecs = numpy.empty((0, count), dtype=type(v))
 
-print(eigvals)
+vt = v.transpose()
+for i in range(k):
+    print(i)
+    a = w.max()
+    print("max:", a)
+    eigvals = numpy.append(eigvals, [a])
+    index = numpy.where(w == a)
+    print("idx:", index)
+    w[index] = 0
+    vec = vt[index]
+    print("vec:", vec)
+    eigvecs = numpy.concatenate((eigvecs, vec), axis = 0)
+
+# eigvals = lambda
+print("eigvals:", eigvals)
+# eigvecs = v
+print("eigvecs (still transposed):", eigvecs)
+
+# ui = A.vi
+u = numpy.empty((0, count), dtype=type(v))
+for i in range(k):
+    # ambil row vi, terus transpose, terus dikali A, hasilnya langsung ui yang N^2 x 1 (A: N^2 x M, vi : M x 1)
+    vi = eigvecs[i].transpose()
+    print(vi)
+    print(vi.size)
+    print(npdata.size)
+    ui = numpy.matmul(npdata, vi)
+    u = numpy.concatenate((u, ui), axis = 1)
+
+print("u:", u)
+
+
+
+
 
 # contoh meanface
 testmeanface = mean.reshape(-1, 256).T
