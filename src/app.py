72fd94a8f714686f741e793b8f752c72741bb939
bangkitdc <bangkitdc@gmail.com>
2022-11-18 13:32:31 +0700

    merging

--- src/app.py
+++ src/app.py
@@ -4,9 +4,7 @@ from tkinter import filedialog
 import cv2
 import numpy
 import os
-
-# def btn_clicked():
-#     print("Button Clicked")
+import time
 
 window = Tk()
 
@@ -146,6 +144,8 @@ def start():
     global imageRes
     data = []
 
+    start_time = time.time()
+
     for imgname in os.listdir(pathdataset):
         print(f'Processing {imgname}')
         img = cv2.imread(os.path.join(pathdataset, imgname), cv2.IMREAD_GRAYSCALE)
@@ -162,6 +162,170 @@ def start():
         d = numpy.subtract(d, mean)
 
     print("done")
+
+    # A = N^2 x M
+    # A = [a1, a2, .., am], A = data
+    npdata = numpy.array(data)
+    # karena masih M x N^2
+    # print(npdata.shape)
+    # A : N^2 x M
+    npdata = npdata.transpose()
+    # print('1')
+    # A : N^2 x M, At : M x N^2
+    # M x M : At x A
+    Cov = numpy.matmul(npdata.transpose(), npdata)
+    # print('2')
+
+    w, v = numpy.linalg.eig(Cov)
+    # print("w")
+    # print(w)
+    # print("v")
+    # print(v.shape)
+    # print(v)
+    # print('3')
+
+    # Now we select k = 5 of w ( max eigenvalues )
+    k = 5
+    # eigvals = numpy.array([])
+    # eigvecs = numpy.empty((0, count), dtype=type(v))
+
+    eigvals, eigvecs = getKeigen(k, w, v, count)
+    # vt = v.transpose()
+    # for i in range(k):
+    #     print(i)
+    #     a = w.max()
+    #     print("max:", a)
+    #     eigvals = numpy.append(eigvals, [a])
+    #     index = numpy.where(w == a)
+    #     print("idx:", index)
+    #     w[index] = 0
+    #     vec = vt[index]
+    #     print("vec:", vec)
+    #     eigvecs = numpy.concatenate((eigvecs, vec), axis = 0)
+
+    # eigvals = lambda
+    # print("eigvals:", eigvals)
+    # eigvecs = v
+    # print("eigvecs (still transposed):", eigvecs)
+
+    # ui = A.vi
+    u = numpy.empty((0, 256*256), dtype=type(v))
+    for i in range(k):
+        # ambil row vi, terus transpose, terus dikali A, hasilnya ui yang N^2 x 1 (A: N^2 x M, vi : M x 1)
+        # ditranspose biar gampang masukinnya
+        vi = eigvecs[i].transpose()
+        # print("vi: ")
+        # print(vi)
+        # print(vi.size)
+        # print(npdata.size)
+        ui = numpy.matmul(npdata, vi)
+        ui = ui.transpose()
+        # print("u", u)
+        # print("ui", ui)
+        # print("size ui:", ui.size)
+        u = numpy.vstack((u, ui))
+        # u = numpy.concatenate((u, ui), axis = 0)
+
+    # print("u:", u)
+    # u = u.transpose()
+    AT = npdata.transpose()
+
+
+    w = numpy.empty((0, k), dtype = type(u))
+    # ui dimensinya 1 x N^2 karena ditranspose
+    # AT dimensinya M x N^2, diambil row nya jadi 1 x N^2
+    # Kaliin semua row (gambar) dengan semua row (ui) matriks u
+    # 1 gambar dikali k u, hasilnya ada k weight, vektor wi dimensinya 1 x k
+    # ada sebanyak count gambar, matriks w jadinya count x k, nanti ditranspose jadi k x count
+    for i in range(count):
+        wi = numpy.empty(0, dtype = type(u)) 
+        Ai = AT[i]
+        for j in range(k):
+            uj = u[j]
+            temp = numpy.dot(Ai, uj)
+            wi = numpy.append(wi, temp)
+            # print(wi.size)
+            # print(wi)
+        w = numpy.vstack((w, wi))
+
+    # print("w:", w)
+    # count x k
+    print(w.size)
+    w = numpy.transpose(w)
+    # k x count
+    print(w.size)
+
+    inputimage = imageInput
+
+    # penyocokan image
+    # test.w : k x count (data set file)
+    wdata = numpy.transpose(w)
+    # wdata : count x k
+
+    # u local : k x N^2
+    ulocal = u
+
+    image = numpy.transpose(inputimage)
+    # image : 1 x N^2
+
+    # ui dimensinya 1 x N^2 karena ditranspose
+    # AT dimensinya M x N^2, diambil row nya jadi 1 x N^2
+    # Kaliin semua row (gambar) dengan semua row (ui) matriks u
+    # 1 gambar dikali k u, hasilnya ada k weight, vektor wi dimensinya 1 x k
+    # ada sebanyak count gambar, matriks w jadinya count x k, nanti ditranspose jadi k x count
+
+    wtest = numpy.empty(0, dtype = type(ulocal)) 
+    for j in range(k):
+        uj = ulocal[j]
+        temp = numpy.dot(image, uj)
+        wtest = numpy.append(wtest, temp)
+        # print(wi.size)
+        # print(wi)
+
+    # wtest adalah weight dari test image jika dibandingkan dengan vektor u
+    # vektor u adalah eigen vector
+
+    # Cari euclidean distance terkecil dari w test dengan w data
+    distance = numpy.linalg.norm(wtest - wdata[0])
+    indexmin = 0
+    for i in range(1, count):
+        temp = numpy.linalg.norm(wtest - wdata[i])
+        if (temp < distance):
+            distance = temp
+            indexmin = i - 1
+    # distance adalah jarak euclidean terkecil
+    # indexmin adalah index dengan w di wdata terdekat dengan wtest
+    print(distance)
+    print(indexmin)
+    print(f'file count: {count}')
+
+    print("donedone")
+
+    current_time = time.time()
+    execution_time = '{:.4f}'.format(current_time - start_time)
+
+    current = entry0.get()
+    entry0.delete(0, END)
+    entry0.insert(0, execution_time + "s")
+
+def getKeigen(k, eigvalues, eigvectors, filecount):
+    # eigvalues and eigvectors are numpy arrays
+    eigvals = numpy.array([])
+    eigvecs = numpy.empty((0, filecount), dtype=type(eigvectors))
+    vt = eigvectors.transpose()
+    for i in range(k):
+        print(i)
+        a = eigvalues.max()
+        print("max:", a)
+        eigvals = numpy.append(eigvals, [a])
+        index = numpy.where(eigvalues == a)
+        print("idx:", index)
+        eigvalues[index] = 0
+        vec = vt[index]
+        print("vec:", vec)
+        eigvecs = numpy.concatenate((eigvecs, vec), axis = 0)
+    
+    return eigvals, eigvecs
 
 def select_dataset():
     global pathdataset
@@ -194,10 +358,12 @@ def resize_256(image):
 
     resized_image = cv2.resize(crop_img, dim, interpolation = cv2.INTER_LINEAR)
 
+    # print(f'res {resized_image.shape}')
+
     return resized_image
 
 def select_image():
-    global image
+    global image, imageInput
 
     path = filedialog.askopenfilename()
 
@@ -207,10 +373,15 @@ def select_image():
 
     if len(path) > 0:
         image = cv2.imread(path)
+        grayscale = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
         resized_image = resize_256(image)
+        resized_grayscale = resize_256(grayscale) 
 
-        # grayscale = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+        # print(f'resized {resized_grayscale.shape}')
+        imageInput = numpy.array(resized_grayscale.T).flatten()
+
+        # print(f'grayscale {imageInput.shape}')
 
         image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
 
